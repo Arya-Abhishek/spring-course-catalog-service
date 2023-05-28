f39489950011d43f323a129eab235e7b9d3ed117
ABHISHEK ARYA <arya2001abhishek@gmail.com>
2023-05-28 19:53:38 +0530

    added unit tests

---
+++ src/test/unit/com/kotlinspring/controller/CourseControllerUnitTest.kt
@@ -0,0 +1,68 @@
+package com.kotlinspring.controller
+
+import com.kotlinspring.dto.CourseDTO
+import com.kotlinspring.service.CourseService
+import com.kotlinspring.util.courseDTO
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.web.reactive.server.WebTestClient
+
+@WebMvcTest(controllers = [CourseController::class])
+@AutoConfigureWebTestClient
+class CourseControllerUnitTest {
+
+    @Autowired
+    lateinit var webTestClient: WebTestClient
+
+    @MockkBean
+    lateinit var courseServiceMock: CourseService
+
+    @Test
+    fun addCourse() {
+        val courseDTO = CourseDTO(null, "Build Restful APIs using Kotlin & SpringBoot2", "Development")
+
+        every { courseServiceMock.addCourse(any()) } returns courseDTO(id = 1)
+
+        val savedCourseDTO =  webTestClient
+            .post()
+            .uri("/v1/courses")
+            .bodyValue(courseDTO)
+            .exchange()
+            .expectStatus().isCreated
+            .expectBody(CourseDTO::class.java)
+            .returnResult()
+            .responseBody
+
+        Assertions.assertTrue {
+            savedCourseDTO!!.id != null
+        }
+    }
+
+    @Test
+    fun retrieveAllCourses() {
+
+        every { courseServiceMock.retrieveAllCourses() }.returnsMany(
+            listOf(
+                courseDTO(id = 1),
+                courseDTO(id = 2, name = "Wiremock for Java Developers")
+            )
+        )
+
+        val courseDTOs = webTestClient
+            .get()
+            .uri("/v1/courses")
+            .exchange()
+            .expectStatus().isOk
+            .expectBodyList(CourseDTO::class.java)
+            .returnResult()
+            .responseBody
+
+        println("All the courses: $courseDTOs")
+        Assertions.assertEquals(2, courseDTOs!!.size)
+    }
+}
