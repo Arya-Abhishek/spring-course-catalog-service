fc19cbadbf5dc79e519630e98956f462630de4dc
ABHISHEK ARYA <arya2001abhishek@gmail.com>
2023-05-28 22:39:14 +0530

    implementing custom query using spring boot data jpa

--- src/main/kotlin/com/kotlinspring/repository/CourseRepository.kt
+++ src/main/kotlin/com/kotlinspring/repository/CourseRepository.kt
@@ -4,4 +4,7 @@ import com.kotlinspring.entity.Course
 import org.springframework.data.repository.CrudRepository
 
 interface CourseRepository: CrudRepository<Course, Int> {
+
+    // this can be an implementation of, finding courses based on matching keyword in the name of course
+    fun findByNameContaining(courseName: String): List<Course>
 }
